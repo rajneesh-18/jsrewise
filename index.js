18e74487e22e69bd16d3fd611a7255a7db212bfc
Rajneesh Maurya <112794051+rajneesh-18@users.noreply.github.com>
2024-05-22 18:18:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const accountId = 144555
+let accountEmail = "dojo@google.com"
+var accountPassword = "12345"
+accountCity = "Remote"
+let accountState;
+// accountId = 2
+
+accountEmail = "svfb@gj.com"
+accountPassword = "635734754"
+accountCity = "Ninja"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+let name = "hitesh"
+let age = "18"
+let isLoggedIn = false
+
+//  number => 2 to power 53
+//bigint
+// string => """
+// boolean => true/false
+// null => standalone , object
+// undefined => 
+//symbol => unique
+// object
+
+console.log(typeof null);
